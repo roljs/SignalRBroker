71692ad0b2c8e84510aff1553b7d1540eeb1af78
roljs <roljs@hotmail.com>
2015-08-21 13:53:52 -0700

    Initial check-in with SendToGroup support. Needed for PocketKnife, but general implementation usable in other scenarios.

---
+++ SignalRBroker/BrokerHub.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalRBroker
+{
+    public class BrokerHub : Microsoft.AspNet.SignalR.Hub
+    {
+        public void Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(name, message);
+            System.Web.HttpContextBase httpContext = Context.Request.GetHttpContext();
+
+        }
+
+        public void SendToGroup(string group, string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.Group(group).broadcastMessage(name, message);
+            System.Web.HttpContextBase httpContext = Context.Request.GetHttpContext();
+
+        }
+
+        public override Task OnConnected()
+        {
+            var sessionId = Context.QueryString["sessionId"];
+            if (sessionId != null && sessionId != String.Empty)
+            {
+                Groups.Add(Context.ConnectionId, sessionId);
+            }
+
+            return base.OnConnected();
+        }
+
+
+
+    }
+}
---
+++ SignalRBroker/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SignalRBroker")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SignalRBroker")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0dfce0b4-bf69-46ba-a235-2b821a5a2367")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
